e30a66bab89383b42a3c17c2ab598a49fedc37c1
Abhishek Sharma <abhi13573006@hmail.com>
2026-01-06 19:03:23 +0100

    more changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "heading"}, "hello i am a motherfucker."),React.createElement("h2", {id: "heading"}, "hello i am a motherfucker.")]))
+const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "heading"}, "hello i am h1."),React.createElement("h2", {id: "heading"}, "hello i am h2.")]))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
